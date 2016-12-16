90912129aac0cef1e534305f29f44c4146d546f0
DevDigital <devdigital.cloud@gmail.com>
2016-12-16 10:29:18 +0000

    Start of webpack production build

---
+++ samples/ts/config.js
@@ -0,0 +1,13 @@
+import path from 'path'
+import { webpackService } from '../../src'
+import standard from '../../src/configs/standard'
+
+const config = webpackService
+  .register(standard)
+  .configure()
+  .entry(path.resolve(__dirname, './src/index.tsx'))
+  .output(path.resolve(__dirname, './src'))
+  .tsReactStandard()
+  .get()
+
+export default config
--- samples/ts/server.js
+++ samples/ts/server.js
@@ -1,14 +1,5 @@
-import path from 'path'
+import config from './config'
 import { webpackService } from '../../src'
-import standard from '../../src/configs/standard'
-
-const config = webpackService
-  .register(standard)
-  .configure()
-  .entry(path.resolve(__dirname, './src/index.tsx'))
-  .output(path.resolve(__dirname, './src'))
-  .tsReactStandard()
-  .get()
 
 console.log(config.module)
 
